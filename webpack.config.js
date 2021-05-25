9f5b414d53464f48982b0b0dd8f710118382a829
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-05-25 12:39:42 +0430

    edit image ext

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
                 use: ['style-loader' , 'css-loader']
             },
             {
-                test: /\.(png|jpe?g|gif)/,
+                test: /\.(png|jpe?g|gif)$/,
                 use: [
                     {
                         loader: 'file-loader',
