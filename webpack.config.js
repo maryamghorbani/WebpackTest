92631684467b4db015c80faaaf575ccd6211703d
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-05-27 17:41:11 +0430

    add clean webpack plugin to project

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 
 module.exports = {
@@ -64,6 +65,7 @@ module.exports = {
         }),
         new MiniCssExtractPlugin({
             filename: '[name].[contenthash].css'
-        })
+        }),
+        new CleanWebpackPlugin()
     ]
 }
