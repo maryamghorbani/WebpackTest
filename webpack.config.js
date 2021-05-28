8cf64e976f9f42e3272a3eabbbdd6dac5440dc32
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-05-28 18:16:37 +0430

    fixed bug

--- webpack.config.js
+++ webpack.config.js
@@ -76,7 +76,7 @@ module.exports = (env , { mode }) => {
     let isDevelopment = mode === 'development';
 
     if (isDevelopment) {
-        config.devserver = {
+        config.devServer = {
             contentBase : path.resolve(__dirname,'build'),
             index : 'index.html',
             port : 8888
